eb0517ebe6086ed061e980789f3c95836e430fdb
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-10-21 21:23:29 -0400

    Fixed images

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,13 +2,13 @@ var friends = [
   {
     name: "Jake Fenwick",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/teddy-bear-dog-breeds-cavapoo-1570403851.jpg?crop=0.668xw:1.00xh;0.250xw,0&resize=640:*",
     scores: [1, 4, 5, 2, 2, 1, 3, 4, 2, 1]
   },
   {
     name: "Alyssa Martin",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://www.washingtonpost.com/resizer/kPkFQsQjvSIjfXG-mFXDEpxq6-4=/767x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg",
     scores: [3, 5, 3, 1, 2, 2, 2, 5, 6, 3]
   }
 ];
